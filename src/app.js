7641e32da881582fdf74bbea38e0fc14dd4eeae9
srilekha2452 <srilekha3890@gmail.com>
2025-08-06 17:54:51 +0530

    exploring different route handlers

--- src/app.js
+++ src/app.js
@@ -1,6 +1,12 @@
 const express=require('express')
 const app=express();
+app.use('/hello/123',(req,res)=>{
+    res.send('This is hello from /hello/123')
+})
 app.use('/hello',(req,res)=>{
     res.send('hello from the server');
+})
+app.use('/',(req,res)=>{
+    res.send('This is the root directory')
 })
 app.listen(7777)
