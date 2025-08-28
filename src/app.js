99ca52eae6255933b6f42925aab0d5df492ce4b9
srilekha2452 <srilekha3890@gmail.com>
2025-08-28 11:37:32 +0530

    used express routers

--- src/app.js
+++ src/app.js
@@ -3,127 +3,17 @@ const connectDB=require('./config/dataBase')
 const app=express();
 const User=require('./models/user')
 const bcrypt = require('bcrypt');
-const { validationSignup}=require('./utils/validation')
 const cookieParser = require('cookie-parser')
 const jwt=require('jsonwebtoken')
-const {userAuth}=require('./middlewares/auth')
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.post('/signup',async(req,res)=>{
-    try{
-    const {firstName,lastName,emailId,password}=req.body;
-    validationSignup(req)
-    const passwordHash=await bcrypt.hash(password,10)
-    const user=new User({
-        firstName,
-        lastName,
-        emailId,
-        password:passwordHash
-    })
-    await user.save();
-    res.send('user added successfully')
-    }
-    catch(err){
-        res.status(400).send('Error:'+err.message)
-    }
-});
+const authRouter=require('./routes/auth');
+const profileRouter=require('./routes/profile')
 
-app.post('/login',async(req,res)=>{
-    try{
-    const {emailId,password}=req.body;
-    const user=await User.findOne({emailId:emailId});
-    if(!user){
-        throw new Error('Invalid credentials')
-    }
-    const isPasswordValid=await user.validatePassword(password);
-    if(isPasswordValid){
-        const token=await user.getJWT();
-        res.cookie("token",token,{expires:new Date(Date.now() + 7 * 24 * 60 * 60 * 1000)});
-        res.send('login successful')
-        
-    }
-    else{
-        throw new Error('Invalid credentials')
-    }
-    }
-    catch(err){
-        res.status(400).send('Error:'+err.message)
-    }
-
-})
-
-app.get('/profile',userAuth,async(req,res)=>{
-    try{
-        const user=req.user;
-        res.send(user)
-    }
-    catch(err){
-        res.status(400).send("Eror:"+err.message)
-    }
-})
-
-app.get('/user',async(req,res)=>{
-    try{
-    const userEmail=req.body.emailId
-    const users=await User.find({emailId:userEmail})
-    if(users.length===0){
-        res.status(400).send('user not found')
-    }
-    else{
-    res.send(users)
-    }
-    }
-    catch(err){
-        res.status(400).send('error occured',err.message)
-    }
-})
-
-app.get('/feed',async(req,res)=>{
-    try{
-        const user=await User.find({})
-        if(!user){
-            res.status(400).send('users not found');
-        }
-        else{
-            res.send(user)
-        }
-    }
-    catch(err){
-        res.status(400).send('error occured',err.message)
-    }
-})
-app.patch('/user/:userId',async(req,res)=>{
-    const userId=req.params?.userId;
-    const data=req.body;
-    try{
-        const ALLOWED_UPDATES=[
-            "userId",
-            "photoUrl",
-            "about",
-            "gender",
-            "age",
-            "skills"
-        ];
-        const isUpdateAllowed=Object.keys(data).every((k)=>
-        ALLOWED_UPDATES.includes(k));
-        console.log(isUpdateAllowed) 
-        if(!isUpdateAllowed){
-            throw new Error('Update not allowed')
-        }
-        if(data?.skills.length>10){
-            throw new Error('skills cannot be exceeded more than 10')
-        }
-        
-        await User.findByIdAndUpdate({_id:userId},data,{runValidators:true,returnDocument:"after"})
-        res.send('user details updated successfully');
-        
-    }
-    catch(err){
-        res.send('something went wrong-'+err.message)
-    }
-})
+app.use('/auth', authRouter);
+app.use('/profile', profileRouter);
 
 
 connectDB().then(()=>{
